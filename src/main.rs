1b4c10a53eea7d122d01e63a17ab3760de2d6209
Sergei <34352375+mrlanu@users.noreply.github.com>
2022-10-31 02:11:00 +0000

    feat: add start confirmation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use spammer::Messanger;
+use std::io;
 
 fn main() {
-    Messanger::build().run().expect("Error");
+    println!("Do you really want to start ? y/n");
+  
+    let mut answer = String::new();
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("Filed to read line");
+  
+    match &answer.trim()[..] {
+        "y" => {
+            println!("Starting");
+            Messanger::build().run().expect("Error");
+        },
+        _ => println!("Bye"),
+    }
 }
